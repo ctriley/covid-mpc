94c2a2cb9b4d3b3160e39a1d74b25c0870fd313a
Connor Riley <ctriley@gatech.edu>
2020-03-31 23:22:14 -0400

    working model

--- mpc.py
+++ mpc.py
@@ -5,82 +5,100 @@ from gurobipy import *
 
 def main():
     cities = ["new_york", "chicago"]
-    travel_times = {"new_york": {"chicago": 1}}
+    travel_times = {"new_york": {"chicago": 1, "new_york": 0}, "chicago": {"new_york": 1, "chicago": 0}}
     demand_prediction = {"new_york": [5,3,2,1], "chicago": [2,3,4,2]}
-    resources {"new_york": [5, 4], "chicago": [3,7] }
-    lookahead = 4
+    resources = {"new_york": [5, 4, 0, 0], "chicago": [3,7, 0, 0] }
+    lookahead = 2
     length_of_resource_need = 1
     death_rate = .8
-    run_model(cties, travel_times, demand_prediction, resources, 
+    print([x for x in range(0, length_of_resource_need)])
+    print("#########################")
+    run_model(cities, travel_times, demand_prediction, resources, 
         lookahead, death_rate, length_of_resource_need)
     
 
 def run_model(cities, travel_times, demand_prediction, resources, lookahead, 
     death_rate, length_of_resource_need):
     try:
-        m = Model("covid-mpc")
+        model = Model("covid-mpc")
 
         # variables 
-        xps = [[] for x in xrange(lookahead)]
-        xrs = [[] for x in xrange(lookahead)]
-        vs = [[] for x in xrange(lookahead)]
-        us = [[] for x in xrange(lookahead)]
-        for i in xrange(lookahead):
-            for city in cities:
-                xps[i].append(model.addVar(vtype=GRB.INTEGER))
-                obj_value = lookahead - i
-                us[i].append(model.addVar(vtype=GRB.INTEGER, obj=obj_value))
-                vs[i].append(model.addVar(vtype=GRB.INTEGER))
-        for i in range(0, lookahead):
-            for c1 in cities:
+        xps = [[] for x in range(0,lookahead)]
+        xrs = [[] for x in range(0,lookahead)]
+        vs = [[] for x in range(0,lookahead)]
+        us = [[] for x in range(0,lookahead)]
+        for t in range(0, lookahead):
+            for i, city in enumerate(cities):
+                xps[t].append(model.addVar(name="xp_" + str(i) + str(t), 
+                    vtype=GRB.INTEGER))
+                obj_value = lookahead - t
+                us[t].append(model.addVar(name="u_" + str(i) + str(t), 
+                    vtype=GRB.INTEGER, obj=obj_value))
+                vs[t].append(model.addVar(name="v_" + str(i) + str(t), 
+                    vtype=GRB.INTEGER))
+        for t in range(0, lookahead):
+            for i, c1 in enumerate(cities):
                 t2 = []
-                for c2 in cities:
-                    obj_value = travel_times[i][j]
-                    t2.append(model.addVar(vtype=GRB.INTEGER, obj=obj_value))
-                xrs[i].append(t2)
-
+                for j, c2 in enumerate(cities):
+                    obj_value = travel_times[c1][c2]
+                    t2.append(model.addVar(name="xr_" + str(i) + str(j) + str(t),
+                        vtype=GRB.INTEGER, obj=obj_value))
+                xrs[t].append(t2)
+        model.update()
         # constraints
 
         #1b
         for i, city in enumerate(cities):
-            model.addConstr(xp[0][i] + us[0][i] == demand_prediction[city][0])
+            model.addConstr(xps[0][i] + us[0][i] == demand_prediction[city][0],
+                    name="1b_" + str(i))
         
         for t in range(1, lookahead):
             for i, city in enumerate(cities):
                 v_temp = []
-                for k in xrange(length_of_resource_need):
-                    if i - k >=0:
-                        v_temp.append(vs[t][i-k])
+                if t - 1 >= 0:        
+                    v_temp.append(vs[t-1][i])
                 ones = [1] * len(v_temp)
                 expr = LinExpr(ones, v_temp)
                 #1c
-                model.addConstr(x[t][i] + us[t][i] == demand_prediction[city][t] - expr)
+                model.addConstr(xps[t][i] + us[t][i] == demand_prediction[city][t] - expr,
+                        name="1c_" + str(i) + str(t))
+        for t in range(0, lookahead):
+            for i, city in enumerate(cities):
                 #1d
-                model.addConstr(vs[t][i] >= death_rate * u[t][i])
-                model.addConstr(vs[t][i] <= death_rate * u[t][i] + 1)
+                model.addConstr(vs[t][i] >= death_rate * us[t][i],
+                        name="1d_1_" + str(i) + str(t))
+                model.addConstr(vs[t][i] <= death_rate * us[t][i] + 1,
+                        name="1d_2_" + str(i) + str(t))
                 #1e
                 v_temp = []
+                ones = []
+                v_temp.append(xps[t][i])
+                ones.append(1.0)
                 for j, c2 in enumerate(cities):
-                    tt = travel_times[j][i]
+                    v_temp.append(xrs[t][i][j])
+                    ones.append(1.0)
+                    tt = travel_times[c2][city]
                     if t - tt >= 0:
                         v_temp.append(xrs[t-tt][j][i])
+                        ones.append(-1.0)
                     if t > 0:
                         v_temp.append(xps[t-1][i])
-                ones = [1] * len(v_temp)
+                        ones.append(-1.0)
                 expr = LinExpr(ones, v_temp)
-                model.addConstr(xps[t][i] + xrs[t][i][j] == resources[city][t] + expr)
+                model.addConstr(expr == resources[city][t],
+                        name="1e_" + str(i) + str(t))
 
-        m.optimize()
-
-        for v in m.getVars():
+        model.optimize()
+        model.write("model.lp")
+        for v in model.getVars():
             print('%s %g' % (v.varName, v.x))
 
-        print('Obj: %g' % m.objVal)
+        print('Obj: %g' % model.objVal)
 
     except GurobiError as e:
         print("Error code " + str(e.errno) + ":" + str(e))
-    except AttributeError:
-        print("Encounter an attribute error")
+    except AttributeError as e:
+        print("Encounter an attribute error" + str(e))
 
 
 if __name__ == "__main__":
